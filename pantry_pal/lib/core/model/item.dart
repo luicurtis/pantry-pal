25d23d745f4499a4ee9d7b43d7c31a196489ed5c
cwlui <cwlui@sfu.ca>
2021-01-02 10:45:48 -0800

    Finished Item model

---
+++ pantry_pal/lib/core/model/item.dart
@@ -0,0 +1,30 @@
+class Item {
+  String id;
+  String name;
+  String shelfNum;
+  int quantity;
+  DateTime lastUpdated;
+
+  // Constructor for Assignment to members
+  Item({this.id, this.name, this.shelfNum, this.quantity, this.lastUpdated});
+
+  // Named Constructor
+  // Maps data from Firebase JSON format to Class Item format
+  Item.fromMap(Map snapshot, String id) :
+    id = id ?? '', // NOTE: '??' means if null
+    name = snapshot['name'] ?? 'New Item',
+    shelfNum = snapshot['shelfNum'] ?? 'N/A',
+    quantity = snapshot['quantity'] ?? 0,
+    lastUpdated = snapshot['lastUpdated'] ?? DateTime.now();
+
+  // Converts Item to JSON format for upload to Firebase
+  Map toJSON() {
+    return {
+      "id": id,
+      "name": name,
+      "shelfNum": shelfNum,
+      "quantity": quantity,
+      "lastUpdated": lastUpdated,
+    };
+  }
+}
