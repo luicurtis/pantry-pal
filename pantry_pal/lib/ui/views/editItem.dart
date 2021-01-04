8f0ace2f52178cd4b5ebdddad45212ac2b4375ca
cwlui <cwlui@sfu.ca>
2021-01-03 19:18:24 -0800

    Added Item details and editing the item

---
+++ pantry_pal/lib/ui/views/editItem.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pantry_pal/core/model/item.dart';
+import 'package:pantry_pal/core/viewmodels/inventory.dart';
+import 'package:provider/provider.dart';
+
+class EditItem extends StatefulWidget {
+  final Item item;
+
+  EditItem({this.item});
+
+  @override
+  _EditItemState createState() => _EditItemState();
+}
+
+class _EditItemState extends State<EditItem> {
+  final _formKey = GlobalKey<FormState>();
+  String name = '';
+  int quantity = 0;
+  String shelfNum = '';
+
+  @override
+  Widget build(BuildContext context) {
+    final itemProvider = Provider.of<Inventory>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Edit Item Details')),
+        actions: [
+          FlatButton(
+            onPressed: () async {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+                Item updatedItem = Item(
+                    name: name,
+                    quantity: quantity,
+                    shelfNum: shelfNum,
+                    lastUpdated: DateTime.now());
+                await itemProvider.updateItem(updatedItem, widget.item.id);
+                Navigator.pop(context);
+              }
+            },
+            child: Text('SAVE'),
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              initialValue: widget.item.name,
+              decoration: InputDecoration(
+                hintText: 'Item Name',
+                fillColor: Colors.grey[300],
+                filled: true,
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter the item name';
+                }
+                return null;
+              },
+              onSaved: (value) => name = value,
+            ),
+            TextFormField(
+              initialValue: widget.item.quantity.toString(),
+              decoration: InputDecoration(
+                hintText: 'Number of Items',
+                fillColor: Colors.grey[300],
+                filled: true,
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please the number of items';
+                }
+                return null;
+              },
+              onSaved: (value) => quantity = int.parse(value),
+            ),
+            TextFormField(
+              initialValue: widget.item.shelfNum,
+              decoration: InputDecoration(
+                hintText: 'Shelf',
+                fillColor: Colors.grey[300],
+                filled: true,
+              ),
+              onSaved: (value) => shelfNum = value,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- pantry_pal/lib/ui/views/itemDetails.dart
+++ pantry_pal/lib/ui/views/itemDetails.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pantry_pal/core/model/item.dart';
+import 'package:pantry_pal/core/viewmodels/inventory.dart';
+import 'package:pantry_pal/ui/views/editItem.dart';
+import 'package:provider/provider.dart';
 
 class ItemDetails extends StatelessWidget {
   final Item itemDetails;
@@ -8,8 +11,45 @@ class ItemDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final itemProvider = Provider.of<Inventory>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${itemDetails.name}'),
+        actions: [
+          IconButton(
+            iconSize: 35,
+            icon: Icon(Icons.delete_forever),
+            onPressed: () async {
+              await itemProvider.removeItem(itemDetails.id);
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 50,
+            child: Center(child: Text('Quantity: ${itemDetails.quantity}')),
+          ),
+          Container(
+            height: 50,
+            child: Center(child: Text('Shelf Number: ${itemDetails.shelfNum}')),
+          ),
+          Container(
+            height: 50,
+            child:
+                Center(child: Text('Last Updated: ${itemDetails.lastUpdated}')),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) => EditItem(item: itemDetails)));
+        },
+        child: Icon(Icons.edit),
+      ),
     );
   }
 }
