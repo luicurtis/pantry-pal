27167f2e2b968071e458ca6d132463205b0dd559
cwlui <cwlui@sfu.ca>
2021-12-31 09:22:46 -0800

    removed old folder and files

--- old/constants.dart
+++
@@ -1,9 +0,0 @@
-class Constants {
-  static const String Edit = 'Edit';
-  static const String Delete = 'Delete';
-
-  static const List<String> ItemChoices = <String> [
-      Edit,
-      Delete
-  ];
-}
