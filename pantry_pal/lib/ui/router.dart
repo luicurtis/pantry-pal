fa042089cba014122f87f8d5f07e2a66567a2114
cwlui <cwlui@sfu.ca>
2022-01-01 14:06:24 -0800

    removed paths that are not in use
    NOTE: may not need this file

--- pantry_pal/lib/ui/router.dart
+++ pantry_pal/lib/ui/router.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pantry_pal/ui/views/addItem.dart';
-import 'package:pantry_pal/ui/views/editItem.dart';
+// import 'package:pantry_pal/ui/views/editItem.dart';
 import 'package:pantry_pal/ui/views/home.dart';
-import 'package:pantry_pal/ui/views/itemDetails.dart';
+// import 'package:pantry_pal/ui/views/itemDetails.dart';
 
+// FIXME: Not sure if this is being used or not .. needs more investigation
 class UIRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -11,10 +12,11 @@ class UIRouter {
         return MaterialPageRoute(builder: (_) => Home());
       case '/addItem':
         return MaterialPageRoute(builder: (_) => AddItem());
-      case '/itemDetails':
-        return MaterialPageRoute(builder: (_) => ItemDetails());
-      case '/editItem':
-        return MaterialPageRoute(builder: (_) => EditItem());
+      // Path not used
+      // case '/itemDetails':
+      //   return MaterialPageRoute(builder: (_) => ItemDetails(itemDetails: null,));
+      // case '/editItem':
+      //   return MaterialPageRoute(builder: (_) => EditItem());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
